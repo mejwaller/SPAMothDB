fb72a97e989b367156cb69c6e67007cc25168bfa
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2016-05-06 11:09:45 +0100

    Add basic model anc ocntroller scaffolding

---
+++ MothWeb/MothWeb/Models/Record.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MothWeb.Models
+{
+    public class TaxonRecord
+    {
+        public string CommonName { get; set; }
+        public string Order { get; set; }
+        public string Family { get; set; }
+        public string SubFamily { get; set; }
+        public string Genus { get; set; }
+        public string Species { get; set; }
+        public string SubSpecies { get; set; }
+        public string Aberration { get; set; }
+        public string Form { get; set; }
+        public int count { get; set; }
+        public string notes { get; set; }
+
+    }
+
+    public class RecordEvent
+    {
+        public DateTime Date { get; set; }
+        public string RecordType { get; set; }
+        public string GridRef { get; set; }
+        public string Notes { get; set; }
+        public TaxonRecord[] Records { get; set; }
+
+    }
+}
