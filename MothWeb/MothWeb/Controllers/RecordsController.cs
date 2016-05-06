f8ae3bd35c1574cb4626f4829e1b8743719ae4c5
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2016-05-06 15:54:08 +0100

    Add basic record date chsoign functioanoity using static record event list

--- MothWeb/MothWeb/Controllers/RecordsController.cs
+++ MothWeb/MothWeb/Controllers/RecordsController.cs
@@ -5,9 +5,28 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using MothWeb.Models;
+
 namespace MothWeb.Controllers
-{
+{    
     public class RecordsController : ApiController
     {
+        RecordEvent[] records = new RecordEvent[]
+        {
+            new RecordEvent{Date = new DateTime(2016,05,04)},
+            new RecordEvent{Date = new DateTime(2016,05,05)},
+            new RecordEvent{Date = new DateTime(2016,04,01),RecordType = "one type"},
+            new RecordEvent{Date = new DateTime(2016,04,01),RecordType="another type same date"}
+        
+        };
+
+        public IEnumerable<RecordEvent> GetAllDates()
+        {
+            return records;
+        }
+        public IHttpActionResult GetRecord(DateTime date)
+        {
+            return NotFound();
+        }
     }
 }
